dbb6249c37b4132b3e4114fedd5d3ad26a5bec46
Siddartha-M <77923013+Siddartha-M@users.noreply.github.com>
2021-04-17 23:47:47 +0530

    Update train_utils.py

--- utils/train_utils.py
+++ utils/train_utils.py
@@ -17,7 +17,7 @@ def train_conv_nets(
     convnet_depth,
     convnet_widths,
     label_noise_as_int=10,
-    n_batch_steps=250_000,
+    n_batch_steps=500_000,
     optimizer=None,
     save=True,
     data_save_path_prefix="",
